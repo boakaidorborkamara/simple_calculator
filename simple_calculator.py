6561f3d96589507cdc8dcca5b746b89db0a6c38d
Boakai <boakaidkamara@gmail.com>
2022-05-19 15:36:24 +0000

    getting first and second number from user including operator

--- simple_calculator.py
+++ simple_calculator.py
@@ -3,15 +3,25 @@ def calculate_numbers():
     #get first numbers from user
     num1 = int(input("Enter first number \n"))
 
-    #get operator
-    print("Select an opearator")
-    opearator = (input("+\n-\n/\n*\n%\n"))
 
+    #acceptable operators
+    operators = ["+", "-", "/", "*", "%"]
+
+
+    #prompt user to select a valid operator
+    print("Select an operator")
+    selected_operator = (input("+,-,/,*,% \n"))
+    while(selected_operator not in operators):
+        #keep prompting until a valid operator is enter
+        selected_operator = (input("+,-,/,*,% \n")) 
+
+
+    #get second number
     num2 = int(input("Enter first number \n"))
 
+
+    #execute operations base on selected operator
     
-    operator = (input("Choose operator"))
-    print(operator)
 
     
 
