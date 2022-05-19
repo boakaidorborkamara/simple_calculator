7b9aded21ac016cbb1c338e6d4584264e4e4779d
Boakai <boakaidkamara@gmail.com>
2022-05-19 14:41:39 +0000

    displaying operators for users to choose from

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,6 +1,19 @@
 def calculate_numbers():
+
+    #get first numbers from user
     num1 = int(input("Enter first number \n"))
+
+    #get operator
+    print("Select an opearator")
+    opearator = (input("+\n-\n/\n*\n%\n"))
+
     num2 = int(input("Enter first number \n"))
+
+    
+    operator = (input("Choose operator"))
+    print(operator)
+
+    
 
 
 # call calculator function 
